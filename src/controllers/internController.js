8688b7da7ce45d84d12a2296d6cee01179408ba9
Gaikawade <gmhs6199@gmail.com>
2022-06-29 20:55:00 +0530

    create intern API written successful

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -13,7 +13,10 @@ const createIntern = async function (req, res) {
   try {
     let data = req.body;
 
-    const { name, email, mobile, collegeName } = data;
+    const { name, email, mobile, collegeName, isDeleted } = data;
+    if(!isDeleted){
+      data.isDeleted = false;
+    }
 
     if (Object.keys(data).length == 0) {
       return res.status(400).send({ status: false, msg: "You must enter data." });
@@ -48,13 +51,21 @@ const createIntern = async function (req, res) {
       return res.status(400).send({status: false, msg: "Mobile no. is already present.Enter a new moile no.",});
     }
 
-    let getCollege = await collegeModel.find({_id: collegeName});
+    let getCollege = await collegeModel.findOne({name: collegeName});
     if(!getCollege){
-        return res.status(400).send({status: false, msg: "No college is listed with that CollegeId"});
+        return res.status(400).send({status: false, msg: "No college is listed with that College name"});
     }
 
-    let internData = await internModel.create(data);
-    res.status(201).send({status: true, msg: "Intern Created successfully", data: internData,});
+    let result = {
+      isDeleted: data.isDeleted,
+      name: name,
+      email: email,
+      mobile: mobile,
+      collegeId: getCollege._id
+    }
+
+    let internData = await internModel.create(result);
+    res.status(201).send({status: true, msg: "Intern Created successfully", data: result});
   } catch (err) {
     res.status(500).send({ status: false, msg: err.message });
   }
